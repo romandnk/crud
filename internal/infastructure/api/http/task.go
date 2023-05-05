54d866b67ce2f26539cd8d14cf706eb8a0d55f65
Roman <danilenko_roma@vk.com>
2023-05-05 22:16:12 +0300

    createTask without ctx

--- internal/infastructure/api/http/task.go
+++ internal/infastructure/api/http/task.go
@@ -1,12 +1,31 @@
 package http
 
 import (
+	"encoding/json"
+	"github.com/romandnk/crud/internal/entities/task"
 	"net/http"
 )
 
 func (h *Handler) createTask(w http.ResponseWriter, r *http.Request) {
-	id, err := h.services.Task.Create() // in Create must be context
+	var input task.Task
 
+	err := json.NewDecoder(r.Body).Decode(&input)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	id, err := h.services.Task.Create(ctx, input) // in Create must be context
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	w.WriteHeader(http.StatusCreated)
+	w.Header().Set("Content-Type", "application/json")
+	resp, _ := json.Marshal(map[string]interface{}{
+		"id": id,
+	})
+	w.Write(resp)
+	return
 }
 
 func (h *Handler) getAllTasks(w http.ResponseWriter, r *http.Request) {
